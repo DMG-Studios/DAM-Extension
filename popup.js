fbc6ddaff7e0ab0bd223664bf14f3938316e09c9
nuggex <nuggex@gmail.com>
2021-01-21 22:44:05 +0200

    this doesn't work

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 let date = new Date();
+date.setHours(2, 0, 0, 0);
 let options = { weekday: 'long' }
 let today = date.toLocaleDateString('en-us', options);
 let element = document.getElementById('today');
@@ -22,23 +23,88 @@ function attachClickEvent() {
     }
 }
 
+var food;
+/*
+xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        food = JSON.parse(xmlhttp.response);
+    }
+
+
+
+}
+xmlhttp.open("GET", 'http://nugge.fi/foodGet.php', false);
+xmlhttp.send();
+
+
+/////
+async function fetchFood() {
+    let response = await fetch('http://nugge.fi/foodGet.php');
+
+    console.log(response.status); // 200
+    console.log(response.statusText); // OK
+
+    if (response.status === 200) {
+        let data = await response;
+    }
+}
+
+fetchFood();
+
+
+async function getText() {
+    let myObject = await fetch('http://nugge.fi/foodGet.php');
+    let myText = await myObject.text();
+    console.log(myText);
+  }
+  
+
+
+fetch('http://nugge.fi/foodGet.php').then((response)=>{
+    console.log(response.text)});
+*/
+/*
+fetch('http://nugge.fi/foodGet.php').then(r => food = r.json());
+*/
+
+fetch('http://nugge.fi/foodGet.php').then(r => r.json()).then(r => {showTest(r)})
+var menus;
+function showTest(r){
+    food = r;
+    menus = food.MenusForDays;
+}
+
+var menuList = [];
+
+Object.keys(menus).forEach(function (k) {
+    let menudate = new Date(menus[k].Date.substring(0, 10))
+    if (menudate.toISOString() == date.toISOString()) {
+        for (i = 0; i < menus[k].SetMenus.length; i++) {
+            menuList[i] = menus[k].SetMenus[i].Components;
+        }
+    }
+});
+
 function showLuch() {
-    if (content.style.display == 'block' && content.classList.contains(this.id)) {
+    if (this.id == "today") {
+        content.style.display = 'none';
+    } else if (content.style.display == 'block' && content.classList.contains(this.id)) {
         content.style.display = 'none'
         content.className = '';
     } else {
         if (this.id == 'veg') {
             content.classList.add('veg');
-            content.textContent = vegFood;
+            content.textContent = menuList[0];
         } else if (this.id == 'met') {
             content.classList.add('met');
-            content.textContent = metFood;
+            content.textContent = menuList[1];
         } else if (this.id == 'soup') {
             content.classList.add('soup');
-            content.textContent = soupFood;
+            content.textContent = menuList[2];
         } else if (this.id == 'dess') {
             content.classList.add('dess');
-            content.textContent = dessFood;
+            content.textContent = menuList[3];
         } else if (content.style.display == 'block') {
             content.style.display = 'none';
         }
@@ -47,4 +113,3 @@ function showLuch() {
 }
 
 attachClickEvent();
-
