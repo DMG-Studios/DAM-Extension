438fb37e57d193ef76d185f8565e1343eafc7f01
nuggex <nuggex@gmail.com>
2021-01-21 11:51:36 +0200

    Added JS
    JS adds food functionality pending backend to hide and show selection.

--- popup.js
+++ popup.js
@@ -1,6 +1,50 @@
 let date = new Date();
-let options = { weekday: 'long'}
-let today = date.toLocaleDateString('en-us',options);
+let options = { weekday: 'long' }
+let today = date.toLocaleDateString('en-us', options);
 let element = document.getElementById('today');
 
 element.innerHTML = today;
+
+let vegFood = "Cucumbers with aspargus";
+let metFood = "Beef with haggis";
+let soupFood = "Warm bird water";
+let dessFood = "Sugar infused fatness";
+
+
+var divList = document.getElementsByClassName('lunchbutton');
+var content = document.getElementById('content');
+
+function attachClickEvent() {
+    var listLength = divList.length;
+    var i = 0;
+
+    for (; i < listLength; i++) {
+        divList[i].addEventListener("click", showLuch);
+    }
+}
+
+function showLuch() {
+    if (content.style.display == 'block' && content.classList.contains(this.id)) {
+        content.style.display = 'none'
+        content.className = '';
+    } else {
+        if (this.id == 'veg') {
+            content.classList.add('veg');
+            content.innerHTML = vegFood;
+        } else if (this.id == 'met') {
+            content.classList.add('met');
+            content.innerHTML = metFood;
+        } else if (this.id == 'soup') {
+            content.classList.add('soup');
+            content.innerHTML = soupFood;
+        } else if (this.id == 'dess') {
+            content.classList.add('dess');
+            content.innerHTML = dessFood;
+        } else if (content.style.display == 'block') {
+            content.style.display = 'none';
+        }
+        content.style.display = 'block';
+    }
+}
+
+attachClickEvent();
